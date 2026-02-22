9d5846880ca083868bbcb3a995a4027a47d7f54f
Ilyas <dahhou.ilyas@gmail.com>
2026-02-22 20:07:35 +0000

    build a propr snapshot metric

--- websocket/metricHandler.go
+++ websocket/metricHandler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/dahhou-ilyas/host-container/service"
 	"github.com/dahhou-ilyas/host-container/utils"
@@ -64,18 +65,52 @@ func (mh *MetricHandler) WsHandler(w http.ResponseWriter, r *http.Request) {
 	defer ioReader.Close()
 
 
+
 	for {
         var v container.StatsResponse
+
         if err := decoder.Decode(&v); err != nil {
             break
         }
 
-        //snapshot := buildSnapshot(v)
+        snapshot := buildSnapshot(v)
 
-        if err := conn.WriteJSON(v); err != nil {
+
+        if err := conn.WriteJSON(snapshot); err != nil {
             break
         }
     }
+}
+
+type MetricsSnapshot struct {
+	Timestamp   time.Time   `json:"timestamp"`
+	CPU         float64 `json:"cpu"`        
+	MemoryUsed  uint64  `json:"memoryUsed"` 
+	MemoryTotal uint64  `json:"memoryTotal"`
+	NetworkRx   uint64  `json:"networkRx"`  
+	NetworkTx   uint64  `json:"networkTx"`  
+	BlockRead   uint64  `json:"blockRead"`  
+	BlockWrite  uint64  `json:"blockWrite"` 
+}
+
+func buildSnapshot(current container.StatsResponse) MetricsSnapshot {
+
+	Timestamp := current.Read;
+
+	//Δcontainer
+	deltaTotalUsage := current.CPUStats.CPUUsage.TotalUsage - current.PreCPUStats.CPUUsage.TotalUsage
+	//Δsystem
+	deltaSystemCpuUsage := current.CPUStats.SystemUsage - current.PreCPUStats.SystemUsage
+
+	CPUpercentage := (deltaTotalUsage/deltaSystemCpuUsage) * uint64(current.CPUStats.OnlineCPUs) * 100
+
+
+	MemoryTotal := current.MemoryStats.Limit;
+	MemoryUsage := current.MemoryStats.Usage - current.MemoryStats.Stats["inactive_file"]
+
+
+	return MetricsSnapshot{Timestamp: Timestamp,CPU: float64(CPUpercentage),MemoryUsed: MemoryUsage,MemoryTotal: MemoryTotal}
 
 
 }
+
